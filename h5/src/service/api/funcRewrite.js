0db633c632a681d8aec6a2153fd0f23534e1c52a
luoyushi <luoyushi@jd.com>
2020-01-03 14:48:30 +0800

    修复小程序框架 在iOS上 demo canvas动画无法播放

--- h5/src/service/api/funcRewrite.js
+++ h5/src/service/api/funcRewrite.js
@@ -1,9 +1,8 @@
 // rewrite Function adn SetTimeout setInterval
 
-(function (exports) {
-
-    require('./bridge')
-/*
+;(function (exports) {
+  require('./bridge')
+  /*
     if ("undefined" != typeof Function) {
         Function;
         e = {},
@@ -24,23 +23,90 @@
             })
     }
 */
-    // "undefined" != typeof eval && (eval = void 0),
-    "undefined" != typeof navigator && !function () {
-        var originalSetTimeOut = setTimeout;
-        window.setTimeout = function (fn, timer) {
-            if ("function" != typeof fn) {
-                throw new TypeError("setTimetout expects a function as first argument but got " + typeof(fn) + ".");
-            }
-            var callback = Reporter.surroundThirdByTryCatch(fn, "at setTimeout callback function");
-            return originalSetTimeOut(callback, timer)
-        };
-        var originalSetInterval = setInterval;
-        window.setInterval = function (fn, timer) {
-            if ("function" != typeof fn) {
-                throw new TypeError("setInterval expects a function as first argument but got " + typeof(fn) + ".");
-            }
-            Reporter.surroundThirdByTryCatch(fn, "at setInterval callback function");
-            return originalSetInterval(fn, timer)
+  var userAgent = window.navigator.userAgent
+  var isAndroid = userAgent.indexOf('Android') !== -1
+  var isIOS = !isAndroid
+  if (isIOS) {
+    // iOS setTimeOut需要特别处理
+    var callbackId = 0
+    var callbacks = {}
+    ServiceJSBridge.on(
+      'onSetTimeout',
+      function (params, webviewId) {
+        if (typeof callbacks[params.callbackId] === 'function') {
+          callbacks[params.callbackId]()
         }
-    }()
-}).call(exports, function () { return this }())
+      },
+      'onSetTimeout'
+    )
+    ServiceJSBridge.on(
+      'onSetInterval',
+      function (params, webviewId) {
+        if (typeof callbacks[params.callbackId] === 'function') {
+          callbacks[params.callbackId]()
+        }
+      },
+      'onSetInterval'
+    )
+
+    window.setTimeout = function (fn, timer) {
+      const id = callbackId++
+      callbacks[id] = fn
+      ServiceJSBridge.publish(
+        'setTimeout',
+        { timer: timer, callbackId: id },
+        ''
+      )
+      return id
+    }
+    window.setInterval = function (fn, timer) {
+      const id = callbackId++
+      callbacks[id] = fn
+      ServiceJSBridge.publish(
+        'setInterval',
+        { timer: timer, callbackId: id },
+        ''
+      )
+      return id
+    }
+    window.clearTimeout = function (timerId) {
+      ServiceJSBridge.publish('clearTimeout', timerId, '')
+    }
+    window.clearInterval = function (timerId) {
+      ServiceJSBridge.publish('clearTimeout', timerId, '')
+    }
+  } else {
+    var originalSetTimeOut = setTimeout
+    window.setTimeout = function (fn, timer) {
+      if (typeof fn !== 'function') {
+        throw new TypeError(
+          'setTimetout expects a function as first argument but got ' +
+            typeof fn +
+            '.'
+        )
+      }
+      var callback = Reporter.surroundThirdByTryCatch(
+        fn,
+        'at setTimeout callback function'
+      )
+      return originalSetTimeOut(callback, timer)
+    }
+    var originalSetInterval = setInterval
+    window.setInterval = function (fn, timer) {
+      if (typeof fn !== 'function') {
+        throw new TypeError(
+          'setInterval expects a function as first argument but got ' +
+            typeof fn +
+            '.'
+        )
+      }
+      Reporter.surroundThirdByTryCatch(fn, 'at setInterval callback function')
+      return originalSetInterval(fn, timer)
+    }
+  }
+}.call(
+  exports,
+  (function () {
+    return this
+  })()
+))
